a15831d923b057ea550dc8f68eb7d217ee668d37
Andrea Clement <samuel.dietz@epfl.ch>
2021-12-22 12:58:02 +0100

    added error computations

--- text_distances.py
+++ text_distances.py
@@ -1,4 +1,3 @@
-
 def file_name_to_voc(file_name):
     f = open(file_name, 'r')
     txt = f.read()
@@ -10,19 +9,38 @@ def jaccard(l1, l2):
     s2 = set(l2)
     intersection = s1 & s2
     union = s1 | s2
-    return len(intersection) / len(union)
+    if(len(union) > 0):
+        return (len(intersection) / len(union))
+    else:
+        return 1
 
 
-# main function
 def vocab_distance(file_name1, file_name2):
     voc1 = file_name_to_voc(file_name1)
     voc2 = file_name_to_voc(file_name2)
 
     jacc_vocab = jaccard(voc1, voc2)
 
-    voc1_pos = [(w, i) for i, w in enumerate(voc1)]
-    voc2_pos = [(w, i) for i, w in enumerate(voc2)]
+    old_voc1 = voc1.copy()
+    voc1.insert(0, "0")
+    voc1_pos = list(zip(voc1, old_voc1))
+
+    old_voc2 = voc2.copy()
+    voc2.insert(0, "0")
+    voc2_pos = list(zip(voc2, old_voc2))
 
     jacc_vocab_pos = jaccard(voc1_pos, voc2_pos)
 
     return jacc_vocab, jacc_vocab_pos
+
+def errors(file_name, truth_file):
+    voc = file_name_to_voc(file_name)
+    voc_truth = file_name_to_voc(truth_file)
+
+    s = set(voc)
+    s_truth = set(voc_truth)
+
+    errors = s - s_truth
+    true_vals = s_truth - s
+
+    return errors, true_vals

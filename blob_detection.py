81f979216957d7f63ead83451ddd51b9fb671a19
Julien Sahli <julien.sahli@epfl.ch>
2021-12-23 10:00:04 +0100

    * Blob detection : final image is now an average of the initial and cleaned image

--- blob_detection.py
+++ blob_detection.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np;
 
 
-def clear_image(name, src, dst):
+def clear_image(name, src, dst, merge_image):
 
     # Load image from file
     image = cv2.imread(src + "/" + name)
@@ -39,7 +39,11 @@ def clear_image(name, src, dst):
         for i in range(-s,s+1):
             for j in range(-s,s+1):
                 if (i*i + j*j <= s2) and (y+j >= 0) and (x+i >= 0) and (y+j < height) and (x+i < width):
-                    image[y+j,x+i] = (255,255,255)
+                    if(merge_image):
+                        (b, g, r) = image[y+j,x+i]
+                        image[y+j,x+i] = (128+b/2,128+g/2,128+r/2)
+                    else:
+                        image[y+j,x+i] = (255,255,255)
     #save image
     cv2.imwrite(dst + '/' + name, image)
     return
